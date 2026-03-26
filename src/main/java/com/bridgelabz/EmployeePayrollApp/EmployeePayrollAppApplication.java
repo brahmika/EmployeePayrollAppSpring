f7eb526bc7778373fab287812e4fd52afbb95b9f
brahmika <brahmika30@gmail.com>
2026-03-27 00:28:28 +0530

    [Brahmika] Added: Initial Spring Boot Employee Payroll App setup

--- src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/EmployeePayrollAppApplication.java
@@ -1,4 +1,4 @@
-package com.bridgelabz.EmployeePayrollApp;
+package java.com.bridgelabz.EmployeePayrollApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,4 @@ public class EmployeePayrollAppApplication {
 	}
 
 }
+//
