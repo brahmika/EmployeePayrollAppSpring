f8601819b8525b7c81dc23451378b06cf1b3a31c
brahmika <brahmika30@gmail.com>
2026-03-27 01:11:24 +0530

    [Brahmika] Added: UC5 - Service Layer stores Employee Payroll Data in memory as List

--- src/main/java/com/bridgelabz/EmployeePayrollApp/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/service/EmployeePayrollService.java
@@ -3,36 +3,70 @@ package com.bridgelabz.employeepayrollapp.service;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class EmployeePayrollService
         implements IEmployeePayrollService {
 
+    // In-memory list to store employees
+    private List<EmployeePayrollData> employeePayrollList
+            = new ArrayList<>();
+
+    // Auto increment ID counter
+    private AtomicLong idCounter = new AtomicLong(1);
+
+    // GET all employees
     @Override
     public List<EmployeePayrollData> getEmployeePayrollData() {
-        return null;
+        return employeePayrollList;
     }
 
+    // GET employee by ID
     @Override
     public EmployeePayrollData getEmployeePayrollDataById(
             long empId) {
-        return null;
+        return employeePayrollList.stream()
+                .filter(emp -> emp.employeeId == empId)
+                .findFirst()
+                .orElse(null);
     }
 
+    // CREATE employee
     @Override
     public EmployeePayrollData createEmployeePayrollData(
             EmployeePayrollDTO employeePayrollDTO) {
-        return null;
+        EmployeePayrollData empData = new EmployeePayrollData(
+                idCounter.getAndIncrement(),
+                employeePayrollDTO.getName(),
+                employeePayrollDTO.getSalary()
+        );
+        employeePayrollList.add(empData);
+        return empData;
     }
 
+    // UPDATE employee
     @Override
     public EmployeePayrollData updateEmployeePayrollData(
             long empId, EmployeePayrollDTO employeePayrollDTO) {
-        return null;
+        EmployeePayrollData empData =
+                getEmployeePayrollDataById(empId);
+        if (empData != null) {
+            empData.name = employeePayrollDTO.getName();
+            empData.salary = employeePayrollDTO.getSalary();
+        }
+        return empData;
     }
 
+    // DELETE employee
     @Override
     public void deleteEmployeePayrollData(long empId) {
+        EmployeePayrollData empData =
+                getEmployeePayrollDataById(empId);
+        if (empData != null) {
+            employeePayrollList.remove(empData);
+        }
     }
 }
